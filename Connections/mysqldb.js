f3dd7b28517c1c15c5b3bce53f62c3c82aef9961
harun <harun.khairy@gmail.com>
2020-06-23 22:27:58 +0700

    create sql connection
    new branch

--- Connections/mysqldb.js
+++ Connections/mysqldb.js
@@ -1,29 +1,12 @@
 const mysql=require('mysql')
 
-<<<<<<< Updated upstream
 const db = mysql.createConnection ({
     host:'localhost',
     user:'root',
     password:'Ha22091984',
     database:'final_project',
     port:'3306'
-});
-=======
-// const db=mysql.createConnection({
-//     host : 'localhost',
-//     user : 'root',
-//     password : 'qweasdiop890',
-//     database : 'ecommta',
-//     port : '3306'
-// })
-const db=mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'Ha22091984',
-    database : 'final_project',
-    port : '3306'
 })
->>>>>>> Stashed changes
 
 db.connect(error => {
     if (error) {
