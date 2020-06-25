3453f51687bd80d085d370f306828739d2337902
harun <harun.khairy@gmail.com>
2020-06-25 20:28:51 +0700

    index

--- Connections/mysqldb.js
+++ Connections/mysqldb.js
@@ -1,10 +1,19 @@
 const mysql=require('mysql')
 
+// const db=mysql.createConnection({
+//     host : 'localhost',
+//     user : 'root',
+//     password : 'qweasdiop890',
+//     database : 'ecommta',
+//     port : '3306'
+// })
+
+//========harun=============//
 const db=mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : 'qweasdiop890',
-    database : 'ecommta',
+    password : 'Ha22091984',
+    database : 'from_aldo',
     port : '3306'
 })
 
