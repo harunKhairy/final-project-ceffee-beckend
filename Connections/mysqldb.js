d05869185837b59554b6142153a8d59f38f64c2c
harun <harun.khairy@gmail.com>
2020-06-25 20:29:54 +0700

    koreksi koneksi

--- Connections/mysqldb.js
+++ Connections/mysqldb.js
@@ -9,12 +9,12 @@ const mysql=require('mysql')
 // })
 
 //========harun=============//
-const db=mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'Ha22091984',
-    database : 'from_aldo',
-    port : '3306'
-})
+// const db=mysql.createConnection({
+//     host : 'localhost',
+//     user : 'root',
+//     password : 'Ha22091984',
+//     database : 'from_aldo',
+//     port : '3306'
+// })
 
 module.exports=db
